ea0416326d9360399c675900c570999d01b43e7b
Donghoon Lee <mdhe1248@gmail.com>
2023-01-04 19:05:35 -0500

    Update help comments (because of backslash issue.)

--- src/runANTs.jl
+++ src/runANTs.jl
@@ -1,12 +1,19 @@
 """
 My default registration parameters:
-runAntsRegistration_default(dim, outname, f, m) = `antsRegistration -v -d $dim -o $outname -r initialTransform\[$f, $m, 1\] -m MI\[$f, $m, 1, 32, regular, 0.2\] -t Translation\[0.1\] -c \[100x100x100, 1e-8, 10\] -s 4x2x1mm -f 8x4x1 -m MI\[$f, $m, 1, 32, regular, 0.2\] -t rigid\[0.1\] -c \[100x100x100, 1e-8, 10\] -s 4x2x1mm -f 8x4x1 -m MI\[$f, $m, 1, 32, regular, 0.2\] -t affine\[0.1\] -c \[100x100x100, 1e-8, 10\] -s 4x2x1mm -f 8x4x1`
+
+Note: remove one backslash from every locaion. (e.g. backslash --> nothing, double backslash --> single backslash) 
+
+runAntsRegistration_default(dim, outname, f, m) = `antsRegistration -v -d \$dim -o \$outname -r initialTransform\\[\$f, \$m, 1\\] -m MI\\[\$f, \$m, 1, 32, regular, 0.2\\] -t Translation\\[0.1\\] -c \\[100x100x100, 1e-8, 10\\] -s 4x2x1mm -f 8x4x1 -m MI\\[\$f, \$m, 1, 32, regular, 0.2\\] -t rigid\\[0.1\\] -c \\[100x100x100, 1e-8, 10\\] -s 4x2x1mm -f 8x4x1 -m MI\\[\$f, \$m, 1, 32, regular, 0.2\\] -t affine\\[0.1\\] -c \\[100x100x100, 1e-8, 10\\] -s 4x2x1mm -f 8x4x1`
+
 """
 runAntsRegistration_default(dim, outname, f, m) = `antsRegistration -v -d $dim -o $outname -r initialTransform\[$f, $m, 1\] -m MI\[$f, $m, 1, 32, regular, 0.2\] -t Translation\[0.1\] -c \[100x100x100, 1e-8, 10\] -s 4x2x1mm -f 8x4x1 -m MI\[$f, $m, 1, 32, regular, 0.2\] -t rigid\[0.1\] -c \[100x100x100, 1e-8, 10\] -s 4x2x1mm -f 8x4x1 -m MI\[$f, $m, 1, 32, regular, 0.2\] -t affine\[0.1\] -c \[100x100x100, 1e-8, 10\] -s 4x2x1mm -f 8x4x1`
 
 """
 My default registration parameters:
-runAntsTransform_default(imgw_outname, f, m, tform) = `antsApplyTransforms -d 2 -i $m -r $f -o $imgw_outname -n Linear -t $tform -v 1`
+
+Note: remove one backslash from every locaion. (e.g. backslash --> nothing, double backslash --> single backslash) 
+
+runAntsTransform_default(imgw_outname, f, m, tform) = `antsApplyTransforms -d 2 -i \$m -r \$f -o \$imgw_outname -n Linear -t \$tform -v 1`
 """
 runAntsTransform_default(imgw_outname, f, m, tform) = `antsApplyTransforms -d 2 -i $m -r $f -o $imgw_outname -n Linear -t $tform -v 1`
 
