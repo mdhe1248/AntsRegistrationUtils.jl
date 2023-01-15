070a785abe404d85a75e26026bfa275b8f66d679
Donghoon Lee <mdhe1248@gmail.com>
2023-01-15 12:42:44 -0500

    add transformation with SyN transform.

--- src/runANTs.jl
+++ src/runANTs.jl
@@ -26,6 +26,7 @@ function runAntsRegistrationPairwise(dim, fixedfns, movingfns; tag = false, ants
 		end
 		push!(outnames, outname)
 	end
+	#run registration
 	for i in eachindex(movingfns)
     run(antsRegisterFunc(dim, outnames[i], fixedfns[i], movingfns[i]))
   end
@@ -36,6 +37,9 @@ end
 Run pairwise transformation. Non-linear warping has not been implemented. 
 """
 function runAntsTransformsPairwise(fixedfns, movingfns; outfile_tag = false, tform_tag = false, antsTransformFunc = runAntsTransform_default)
+  tformfns = Vector{String}(undef, 0)
+  syntformfns = Vector{String}(undef, 0)
+  imgw_outnames = Vector{String}(undef, 0)
   for i in eachindex(movingfns)
   	# Assign output image file name
   	if outfile_tag == false
@@ -44,7 +48,7 @@ function runAntsTransformsPairwise(fixedfns, movingfns; outfile_tag = false, tfo
   		outfn = outfile_tag[i]*"_"
   	end
     imgw_outname = outfn*"warped.nrrd"
-  
+		push!(imgw_outnames, imgw_outname)
     # Assign input tform
   	if tform_tag == false
   		tform_fn = splitext(movingfns[i])[1]*"_"
@@ -53,6 +57,16 @@ function runAntsTransformsPairwise(fixedfns, movingfns; outfile_tag = false, tfo
   	end
   	# assign input tform file name
     tform = tform_fn*"0GenericAffine.mat"
-    antsTransformFunc(imgw_outname, fixedfns[i], movingfns[i], tform)
+		syntform = tform_fn*"1Warp.nii.gz"
+		push!(tformfns, tfom)
+		push!(syntformfns, tfom)
   end
+  for i in eachindex(movingfns)
+    if isfile(syntform)
+      run(antsTransformFunc(imgw_outnames[i], fixedfns[i], movingfns[i], tformfns[i], syntformfns[i]))
+    else
+      run(antsTransformFunc(imgw_outnames[i], fixedfns[i], movingfns[i], tformfns[i]))
+    end
+  end
+  return(imgw_outnames)
 end
