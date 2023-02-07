485980feaaa495d41a6a5f67bf594575dd8d21c7
Donghoon Lee <mdhe1248@gmail.com>
2023-02-07 17:41:19 -0500

    export applyAntsTransforms

--- src/AntsRegistrationUtils.jl
+++ src/AntsRegistrationUtils.jl
@@ -3,8 +3,7 @@ module AntsRegistrationUtils
 using AxisArrays, Unitful
 const μm = u"μm"
 
-export setAxis, runAntsRegistration_default, runAntsTransform_default, runAntsRegistrationPairwise, runAntsTransformsPairwise, runAntsTransformsPairwiseSyN
-
+export setAxis, runAntsRegistration_default, runAntsTransform_default, runAntsRegistrationPairwise, runAntsTransformsPairwise, runAntsTransformsPairwiseSyN, applyAntsTransforms
 include("setaxis.jl")
 include("runANTs.jl")
 
