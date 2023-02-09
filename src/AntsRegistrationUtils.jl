213aad340f02e6070249355bf3a18289cba64c11
Donghoon Lee <mdhe1248@gmail.com>
2023-02-09 11:33:59 -0500

    typo

--- src/AntsRegistrationUtils.jl
+++ src/AntsRegistrationUtils.jl
@@ -3,7 +3,7 @@ module AntsRegistrationUtils
 using AxisArrays, Unitful
 const μm = u"μm"
 
-export setAxis, runAntsRegistration_default, runAntsRegistrationPairwise, runAntsTransformsPairwise, runAntsTransformsPairwiseSyN, applyAntsTransforms, runAntsTransform_default, runAntsTransfom_inv_default
+export setAxis, runAntsRegistration_default, runAntsRegistrationPairwise, runAntsTransformsPairwise, runAntsTransformsPairwiseSyN, applyAntsTransforms, runAntsTransform_default, runAntsTransform_inv_default
 unclude("setaxis.jl")
 include("runANTs.jl")
 
