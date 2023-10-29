8be8bd75be062c1af7a43b589d3301d72bb1c162
Donghoon Lee <mdhe1248@gmail.com>
2023-10-29 01:52:16 -0400

    add more functions and struct variables for image registration variables

--- src/AntsRegistrationUtils.jl
+++ src/AntsRegistrationUtils.jl
@@ -3,7 +3,7 @@ module AntsRegistrationUtils
 using AxisArrays, Unitful, Images
 const μm = u"μm"
 
-export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin
+export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin, Regvars, assign_regvars, runAntsRegistrationSyN, runAntsRegistrationAffine, runAntsTransformSyN, runAntsTransformInvFixedSyN, runAntsTransformInvAttnSyN, overlay_boundary, applyAntsTransform
 include("setaxis.jl")
 include("runANTs.jl")
 
