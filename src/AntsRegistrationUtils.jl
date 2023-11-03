d21ab6ab209013767cacd7a8509f189a3d48de42
Donghoon Lee <mdhe1248@gmail.com>
2023-11-02 22:54:57 -0400

    export new functions.

--- src/AntsRegistrationUtils.jl
+++ src/AntsRegistrationUtils.jl
@@ -3,7 +3,8 @@ module AntsRegistrationUtils
 using AxisArrays, Unitful, Images, JLD2
 const μm = u"μm"
 
-export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin, pad_images, Regvars, assign_regvars, save_regvars, runAntsRegistrationSyN, runAntsRegistrationAffine, runAntsTransformSyN, runAntsTransformInvFixedSyN, runAntsTransformInvAttnSyN, applyAntsTransform
+export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin, pad_images, Regvars, assign_regvars, save_regvars, runAntsRegistrationSyN, runAntsRegistrationAffine, runAntsTransformSyN, runAntsTransformInvFixedSyN, runAntsTransformInvAttnSyN, applyAntsTransform, ImageVar, horizontal_flip, assign_imagevars, set_flips, save_image_arranged, save_imagevars
+include("arrange_reg_images.jl")
 include("setaxis.jl")
 include("runANTs.jl")
 
