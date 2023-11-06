35be312d96f99aee6dbf2f167403aee5c5ec641b
Donghoon Lee <mdhe1248@gmail.com>
2023-11-06 14:01:43 -0500

    using ImageView

--- src/AntsRegistrationUtils.jl
+++ src/AntsRegistrationUtils.jl
@@ -1,6 +1,6 @@
 module AntsRegistrationUtils
 
-using AxisArrays, Unitful, Images, JLD2
+using AxisArrays, Unitful, Images, JLD2, ImageView
 const μm = u"μm"
 
 export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin, pad_images, lbl_img_ordering, Regvars, assign_regvars, save_regvars, runAntsRegistrationSyN, runAntsRegistrationAffine, runAntsTransformSyN, runAntsTransformInvFixedSyN, runAntsTransformInvAttnSyN, applyAntsTransform, ImageVar, horizontal_flip, assign_imagevars, set_flips, save_image_arranged, save_imagevars
