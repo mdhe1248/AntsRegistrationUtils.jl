facd69713e46d91671bcf4c09a86f83c3f0eca24
Donghoon Lee <mdhe1248@gmail.com>
2023-11-06 13:56:01 -0500

    export lbl_imge_ordering include pad_images.jl

--- src/AntsRegistrationUtils.jl
+++ src/AntsRegistrationUtils.jl
@@ -3,9 +3,10 @@ module AntsRegistrationUtils
 using AxisArrays, Unitful, Images, JLD2
 const μm = u"μm"
 
-export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin, pad_images, Regvars, assign_regvars, save_regvars, runAntsRegistrationSyN, runAntsRegistrationAffine, runAntsTransformSyN, runAntsTransformInvFixedSyN, runAntsTransformInvAttnSyN, applyAntsTransform, ImageVar, horizontal_flip, assign_imagevars, set_flips, save_image_arranged, save_imagevars
+export setAxis, applyAntsTransforms_01, runAntsRegistration_01, runAntsTransform_01, runAntsTransform_inv, padOrigin, pad_images, lbl_img_ordering, Regvars, assign_regvars, save_regvars, runAntsRegistrationSyN, runAntsRegistrationAffine, runAntsTransformSyN, runAntsTransformInvFixedSyN, runAntsTransformInvAttnSyN, applyAntsTransform, ImageVar, horizontal_flip, assign_imagevars, set_flips, save_image_arranged, save_imagevars
 include("arrange_reg_images.jl")
 include("setaxis.jl")
 include("runANTs.jl")
+include("pad_images.jl")
 
 end
